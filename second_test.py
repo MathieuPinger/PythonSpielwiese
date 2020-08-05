1589957b126ef27e2f61be3913c0a9cbdfaef3fd
Mathieu Pinger <mathieu.pinger@zi-mannheim.de>
2020-08-05 16:21:25 +0200

    Strings

--- second_test.py
+++ second_test.py
@@ -6,3 +6,16 @@ Created on Wed Aug  5 14:13:20 2020
 """
 
 print("what is going on")
+print("cagatay is lol")
+
+message = "I lIkE cHeEsE"
+print(message)
+
+print(message.title())
+message = message.lower()
+print(message)
+
+first_name = "Mathieu"
+last_name = "Pinger"
+
+print(first_name + last_name)
